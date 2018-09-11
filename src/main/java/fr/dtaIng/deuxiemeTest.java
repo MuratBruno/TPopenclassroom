5354e49c962fcb201afa41ea693bcdd2b0cae660
Murat Bruno <murat.bruno35@gmail.com>
2018-09-11 19:49:33 +0200

    premier commit tp open classroom

---
+++ src/main/java/fr/dtaIng/deuxiemeTest.java
@@ -0,0 +1,40 @@
+package fr.dtaIng;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class deuxiemeTest
+ */
+public class deuxiemeTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public deuxiemeTest() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String message = "Transmission de variables : OK !";
+		request.setAttribute( "test", message );
+		this.getServletContext().getRequestDispatcher( "/NewFile.jsp" ).forward( request, response );
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/NewFile.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+ <p>Ceci est une page générée depuis une JSP.</p>
+        <p>
+            <% 
+            String attribut = (String) request.getAttribute("test");
+            out.println( attribut );
+            %>
+        </p>
+</body>
+</html>
